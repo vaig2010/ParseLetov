679f1677104ac889431679f5109269185ba7b2b6
vaig2010 <vaig2010@ya.ru>
2024-04-30 00:10:01 +0400

    Add files via upload

---
+++ LetovParse_py3.py
@@ -0,0 +1,34 @@
+﻿from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import random
+
+def PrettyPrint(all_text):
+    for t in all_text.split('\n'):
+        if 'Песни из этого же альбома или этого автора:' in t:
+            break
+        print(t)
+options = webdriver.EdgeOptions()
+
+options.add_argument("--headless=new")
+options.add_argument('--log-level=3')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+#chrome_driver_path  = r'C:\Python311\Scripts\chromedriver.exe'
+#service = Service(chrome_driver_path)
+driver = webdriver.Edge(options=options)
+driver.get("https://www.gr-oborona.ru/texts/")
+elements = driver.find_elements(by='xpath', value=r'//*[@id="abc_list"]/li[2]/a')
+songLinks = []
+for elem in elements:
+    songLinks.append(elem.get_attribute('href'))
+
+r_song = random.choice(songLinks)
+driver.get(r_song)
+elements = driver.find_elements(by='xpath',value=r'//*[@id="cont"]')
+songname = driver.find_element(by='xpath', value=r'//*[@id="headers"]/h3')
+print(songname.text)
+print()
+for elem in elements:
+    PrettyPrint(elem.text)
+
+driver.quit()
